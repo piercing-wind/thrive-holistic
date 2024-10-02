81e32e975eeece059079bb5a462bf2b2d5ca3687
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-10-02 18:37:06 +0530

    Initial Commit

--- src/lib/coustomloader.ts
+++
@@ -1,5 +0,0 @@
-const customImageLoader = ({ src, width, quality }: { src : any, width : any, quality : any }) => {
-   return `${src}?w=${width}&q=${quality || 75}`;
- };
- 
- export default customImageLoader;
