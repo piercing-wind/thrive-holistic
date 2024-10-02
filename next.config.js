6bf75598622102b80ab56c96ca2246948c972b2f
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-10-02 19:49:35 +0530

    Initial Commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '/thrive-holistic',
+    output: 'export',
+    reactStrictMode  : true,
+};
+
+module.exports = nextConfig;
