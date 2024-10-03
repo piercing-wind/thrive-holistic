83e20d0f83a46922028bcb852a11e6d0fa1e4148
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-10-03 12:43:44 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     reactStrictMode: true,
-    basePath: isProduction ? '/thrive-holistic' : '',
+   //  basePath: isProduction ? '/thrive-holistic' : '',
     output: 'export',
     trailingSlash: true, 
     assetPrefix: isProduction ? 'https://thriveholistic.in' : '',
