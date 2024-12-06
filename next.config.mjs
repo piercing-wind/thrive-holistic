b7c1d2aff837db71fd060bdef512f3f520a0bb19
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-12-06 18:32:42 +0530

    Blogs Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,24 @@
-import { hostname } from 'os';
-
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-   images: {
-      remotePatterns:[
-         {
-            protocol : 'https',
-            hostname : 'dnyvrvurgen90.cloudfront.net',
-         },
-         {
-            protocol : 'https',
-            hostname : 'thriveholistic.in',
-         }
-      ]
-   },
-   //  reactStrictMode: true,
-    basePath: isProduction ? '/thrive-holistic' : '',
-    output: 'export',
-    trailingSlash: true, 
-    assetPrefix: isProduction ? 'https://thriveholistic.in/' : '',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dnyvrvurgen90.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'thriveholistic.in',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  basePath: isProduction ? '/thrive-holistic' : '',
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: isProduction ? 'https://thriveholistic.in/thrive-holistic/' : '',
 };
-
-
 
 export default nextConfig;
